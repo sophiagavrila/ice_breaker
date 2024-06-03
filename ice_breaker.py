50024a662f1708b8c24e17eb4bcedb91250eb4cc
Sophia Gavrila <sophiagavrila@Sophias-MacBook-Pro.local>
2024-06-03 12:23:41 -0400

    get POC up and running

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,29 @@
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+value = os.environ['OPENAI_API_KEY']
+
+information = """
+Alan Mathison Turing OBE FRS (/ˈtjʊərɪŋ/; 23 June 1912 – 7 June 1954) was an English mathematician, computer scientist, logician, cryptanalyst, philosopher and theoretical biologist.[5] Turing was highly influential in the development of theoretical computer science, providing a formalisation of the concepts of algorithm and computation with the Turing machine, which can be considered a model of a general-purpose computer.[6][7][8] He is widely considered to be the father of theoretical computer science.[9]
+"""
+
 if __name__ == "__main__":
-    load_dotenv()
-    print("Hello LangChain!")
-    print("Your API key is: ", os.getenv('OPENAI_API_KEY'))
+   
+    summary_template = """
+        given the information {information} about a person, I want you to create:
+        1. a short story about this person
+        2. two interesting facts about this person
+    """
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": information})
+
+    print(res)
