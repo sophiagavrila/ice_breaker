6a90b02a6e048b960ba7d61a7831290ca60502db
Sophia Gavrila <sophiagavrila@Sophias-MacBook-Pro.local>
2024-06-03 11:44:07 -0400

    resolve env

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,7 @@
-# The following code is an example of a simple Python program that prints a message to the console.  We check if __name__ == '__main__' to ensure that the code is only executed when the script is run directly and not when it is imported as a module.
+from dotenv import load_dotenv
+import os
+
 if __name__ == "__main__":
+    load_dotenv()
     print("Hello LangChain!")
+    print("Your API key is: ", os.getenv('OPENAI_API_KEY'))
