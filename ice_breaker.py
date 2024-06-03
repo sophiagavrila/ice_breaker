1e60d861991d36e96c473b849b26811557358773
Sophia Gavrila <sophiagavrila@Sophias-MacBook-Pro.local>
2024-06-03 11:32:29 -0400

    install langchain and black

---
+++ ice_breaker.py
@@ -0,0 +1,3 @@
+# The following code is an example of a simple Python program that prints a message to the console.  We check if __name__ == '__main__' to ensure that the code is only executed when the script is run directly and not when it is imported as a module.
+if __name__ == "__main__":
+    print("Hello LangChain!")
